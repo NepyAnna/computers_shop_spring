18130523b933dd5ebb52c801e9f9d10003cb9fff
NepyAnna <nepyann@gmail.com>
2024-12-13 20:34:59 +0100

    add properties

---
+++ src/main/java/com/sheoanna/computers_shop_spring/ComputersShopSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sheoanna.computers_shop_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputersShopSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputersShopSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheoanna/computers_shop_spring/ComputersShopSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheoanna.computers_shop_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputersShopSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
