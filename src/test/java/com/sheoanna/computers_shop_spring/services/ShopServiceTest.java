7a59caac4bce19279ae04a52f65e482f2c98f056
NepyAnna <nepyann@gmail.com>
2024-12-18 22:05:53 +0100

    add tests for ShopService

--- src/test/java/com/sheoanna/computers_shop_spring/services/ShopServiceTest.java
+++ src/test/java/com/sheoanna/computers_shop_spring/services/ShopServiceTest.java
@@ -2,22 +2,24 @@ package com.sheoanna.computers_shop_spring.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
-import com.sheoanna.computers_shop_spring.dtos.ComputerDto;
+import org.mockito.junit.jupiter.MockitoExtension;
 import com.sheoanna.computers_shop_spring.dtos.ShopDto;
 import com.sheoanna.computers_shop_spring.models.Computer;
 import com.sheoanna.computers_shop_spring.models.Shop;
 import com.sheoanna.computers_shop_spring.repositories.ShopRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class ShopServiceTest {
     @Mock
     private ShopRepository shopRepository;
@@ -42,16 +44,28 @@ public class ShopServiceTest {
         shop.setId_shop(1L);
         shop.setStoreName("Tech Store");
         shop.setTaxID(123456789);
-        shop.setComputers(Collections.singletonList(computer));
-    }
-
-    @Test
-    void testFindShopById() {
-
+        shop.setComputers(List.of(computer));
     }
 
     @Test
     void testIndex() {
+        when(shopRepository.findAll()).thenReturn(List.of(shop));
+
+        List<ShopDto> result = shopService.index();
+
+        assertEquals(1, result.size());
+        assertEquals(shop.getTaxID(), result.get(0).taxID());
+        verify(shopRepository, times(1)).findAll();
+    }
+    @Test
+    void testFindShopById() {
+        when(shopRepository.findById(1L)).thenReturn(Optional.of(shop));
+
+        ShopDto result = shopService.findShopById(1L);
+
+        assertNotNull(result);
+        assertEquals(shop.getStoreName(), result.storeName());
+        verify(shopRepository, times(1)).findById(1L);
 
     }
 }
